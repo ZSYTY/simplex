4361739d2b0a0407b7c1fb200d9570da9184ae2c
PigSkinBadBad <1714760347@qq.com>
2021-01-12 02:50:49 +0800

    add file in and out

--- main.cpp
+++ main.cpp
@@ -1,15 +1,58 @@
 #include "simplex.hpp"
+#include <fstream>
+#include <sstream>
+#include <string>
 
-int main() {
+int main()
+{
     Simplex simplex;
-    int n = 2, m = 3;
-    double ans = 0;
-    Vector x = {0, 0}, c = {-2, -3}, d = {10, -12, -12};
-    Matrix A = {{1, 1}, {-1, -2}, {-2, -1}};
-    simplex.solveStandardForm(n, m, c, A, d, ans, x);
-    std::cout << ans << std::endl;
-    for (int i = 0; i < n; i++) {
-        std::cout << x[i] << " ";
+    int n = 0, m = 0;
+    double ans = 0, tmp;
+    Vector x, c, b;
+    std::vector<int> d, e;
+    Matrix A;
+
+    std::ifstream infile("F:\\in.txt");
+    infile >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        infile >> tmp;
+        c.push_back(tmp);
+    }
+
+    for (int j = 0; j < m; j++)
+    {
+        A.push_back(*(new Vector()));
+        for (int i = 0; i < n; i++)
+        {
+            infile >> tmp;
+            A[j].push_back(tmp);
+        }
+        infile >> tmp;
+        b.push_back(tmp);
+        infile >> tmp;
+        d.push_back(tmp);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        infile >> tmp;
+        e.push_back(tmp);
+    }
+
+    x.resize(n);
+    SolveResult k = simplex.solveNonStandardForm(n, m, c, A, b, d, e, ans, x);
+
+    std::cout << k << std::endl;
+    if (k == Solvable)
+    {
+        std::cout << ans << std::endl;
+
+        for (int i = 0; i < n; i++)
+        {
+            std::cout << x[i] << " ";
+        }
     }
     std::cout << std::endl;
 
